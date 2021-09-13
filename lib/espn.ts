c88c829990fb288543073e2db48817122643257f
tavariscodes <tavariscodes@gmail.com>
2021-09-13 08:43:59 -0400

    create teamStats method and scriptTag helper function
    Created teamStats method inside of Espn Class also created type definitions for EspnStat
    and a StatQuery. Also created a scriptTagToJson method for parsing script tags returned from espn.com

--- lib/espn.ts
+++ lib/espn.ts
@@ -1,11 +1,10 @@
 import axios, {AxiosRequestConfig} from "axios";
 import * as cheerio from "cheerio";
+import { scriptTagToJson } from './helper';
 
 // create types and methods for espn.com api
 
-interface ApiOptions {
-    
-}
+interface ApiOptions {}
 
 interface EspnApi {
     getDomain(): string; // PROPERTY
@@ -20,7 +19,7 @@ interface Query {
 
 // refactor later :)
 interface EspnObjectType {
-    teams: any;
+    teams: Array<EspnTeam>;
 }
 
 interface EspnTeam {
@@ -33,13 +32,30 @@ interface EspnTeam {
     conference: string;
 }
 
+interface statQuery extends Query {
+    type?: string;   // type of stat i.e. rushing, passing, etc...
+}
+
+interface EspnStat {
+    name: string;
+    displayValue: string;
+    value: number;
+    abbreviation: string;
+    dir: string;
+    hidesort: boolean;
+    category?: string
+}
+
+
 class Espn implements EspnApi {
     private domain: string
+    private headers: {[key: string]: string};
     public getDomain() {
         return this.domain;
     }
     constructor(options: ApiOptions) {
         this.domain = 'https://espn.com';
+        this.headers = { 'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36' }
     }
     /**
      * Returns list of teams by sport
@@ -47,19 +63,12 @@ class Espn implements EspnApi {
     async teams(query: Query): Promise<EspnTeam[]> {
         let options: AxiosRequestConfig = {
             url: `${this.domain}/${query.sport}/teams`,
-            headers: { 'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36' },
+            headers: this.headers,
         }
         return axios(options)
             .then(response => {
                 let $ = cheerio.load(response.data);
-                let teamsObject: EspnObjectType;
-                $('script').each(function () {
-                    const scriptTagContents = $(this).html()
-                    const regex = /(?<=window\[.*?\=).*$/gm
-                    if (scriptTagContents.match(regex)) {
-                        teamsObject = JSON.parse(scriptTagContents.match(regex)[0].replace(/;/g, '')).page.content;    // remove trailing semi colon
-                    }
-                })
+                let teamsObject: EspnObjectType = scriptTagToJson($);
                 let conferenceArray = teamsObject.teams[`${query.sport}`]
                 let allTeams: Array<EspnTeam> = [];
                 for (let x  = 0; x < conferenceArray.length; x++) {
@@ -73,13 +82,60 @@ class Espn implements EspnApi {
             })
             .catch(err => {return(err)});
     }
+ 
+    async teamStats(team: EspnTeam, query?: statQuery): Promise<EspnStat[]> {
+        // refactor
+        let url: string | string[] = team.href.split('/'); url.splice(5, 0, 'stats'); url = url.join('/');
+        let options: AxiosRequestConfig = {
+            url,
+            headers: this.headers,
+        }
+        return axios(options)
+            .then(response => {
+                let $ = cheerio.load(response.data);
+                let statsObject: any = scriptTagToJson($);  // refactor
+                let teamStats: any[] = statsObject.stats.teamStats.team;    // refactor
+                let statsArray: EspnStat[] = []; 
+                if (typeof query !== 'undefined') {
+                    statsArray = teamStats.filter(stat => stat.type === query.type)[0].stats;
+                } else {
+                    // return all stats
+                    statsArray = teamStats.reduce((acc, statObj) => {
+                        statObj.stats.forEach(stat => stat.category = statObj.title);
+                        acc = acc.concat(statObj.stats); 
+                        return acc
+                    }, statsArray);
+                }
+                return statsArray;
+            })
+            .catch(err => {return(err)})
+    }
 }
 
 
+
 const TestEspn = new Espn({});
-TestEspn.teams({sport: 'nfl'})
-    .then(teams => console.log(teams.length))
-    .catch(err => console.log(err.message))
+// TestEspn.teams({sport: 'nfl'})
+//     .then(teams => console.log(teams.length))
+//     .catch(err => console.log(err.message))
+
+// TestEspn.teamStats(
+//     {
+//         id: '10',
+//         href: 'https://www.espn.com/nfl/team/_/name/ten/tennessee-titans',
+//         name: 'Tennessee Titans',
+//         shortName: 'Titans',
+//         abbrev: 'ten',
+//         logo: 'https://a.espncdn.com/combiner/i?img=/i/teamlogos/nfl/500/ten.png&w=80&h=80&cquality=40&scale=crop&location=origin&transparent=true',
+//         conference: 'AFC South'
+//     },
+//     // {
+//     //     sport: 'nfl',
+//     //     type: 'rushing'
+//     // }
+// )
+// .then(stats => console.log(stats))
+// .catch(err => console.log(err))
 
 export default Espn;
 
