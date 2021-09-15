b909a629874835d458d09add89c4ca5a8afc6e62
tavariscodes <tavariscodes@gmail.com>
2021-09-14 22:54:29 -0400

    add useQuery hook to dashboard and render data
    Created gql query for client to send to apolloserver via useQuery hook
    render data returned from hook into component

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 // import MainLayout from '../components/MainLayout';
 import { makeStyles } from '@material-ui/core/styles';
 import { Box, Container, Paper } from '@material-ui/core';
+import { gql, useQuery } from '@apollo/client';
 
 const useStyles = makeStyles({
     root: {
@@ -26,19 +27,51 @@ const useStyles = makeStyles({
     },
     smDataCard: {
         display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
         flex: 1,
         border: '1px solid black',
 
     },
 });
 
+interface Team {
+    id: string
+    href: string
+    name: string
+    shortName: string
+    abbrev: string
+    logo: string
+    conference: string,
+    teams: Team[]
+}
+
+const GET_TEAMS = gql`
+    query {
+    teams(sport: "nba") {
+        name,
+    }
+}
+`
+
 const Dashboard: React.FC = () => {
     const classes = useStyles();
+    const {loading, error, data} = useQuery<Team>(GET_TEAMS);
+    if (loading) {return <div>"loading"</div>}
+    if (error) { return <div>{error}</div> }
+    if (!data) {return <p>Couldn't load your webpage</p>}
     return(
         <div className={classes.root}>
             <Container className={classes.topContainer}>
-                <Paper elevation={3} className={classes.medDataCard}> hi </Paper>
-                <Paper elevation={3} className={classes.smDataCard}> hi </Paper>
+                <Paper elevation={3} className={classes.medDataCard}>
+                
+                </Paper>
+                <Paper elevation={3} className={classes.smDataCard}> 
+                Teams:
+                {data.teams.map(team => {
+                    return <div style={{display: 'flex'}}> {team.name} </div>
+                })} 
+                </Paper>
             </Container>
 
         </div>
