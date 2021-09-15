98fd18e17cedba125316b49e82e4e82942f4fd1f
tavariscodes <tavariscodes@gmail.com>
2021-09-14 22:51:02 -0400

    add react router and apollo server to app
    Added react router and rendered main layout inside app.tsx component
    Added Apollo Client to app created ApolloProvider at root level of app

--- src/index.tsx
+++ src/index.tsx
@@ -4,10 +4,23 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useQuery,
+  gql
+} from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql',
+  cache: new InMemoryCache()
+});
+
 ReactDOM.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>,
+  </ApolloProvider>,
   document.getElementById('root')
 );
 
